a3845926b4811175db71e113f5dce1a2158453dc
Denys Dovhan <denysdovhan@gmail.com>
2024-11-06 14:46:57 +0200

    clean-up with providers

--- src/dashboard/withProviders.tsx
+++ src/dashboard/withProviders.tsx
@@ -4,13 +4,11 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { withDashboard } from "@wix/dashboard-react";
 
 export function withProviders(Component: React.ComponentType) {
-  return withDashboard(function () {
-    return (
-      <WixDesignSystemProvider features={{ newColorsBranding: true }}>
-        <QueryClientProvider client={new QueryClient()}>
-          <Component />
-        </QueryClientProvider>
-      </WixDesignSystemProvider>
-    );
-  });
+  return withDashboard(() => (
+    <WixDesignSystemProvider features={{ newColorsBranding: true }}>
+      <QueryClientProvider client={new QueryClient()}>
+        <Component />
+      </QueryClientProvider>
+    </WixDesignSystemProvider>
+  ));
 }
